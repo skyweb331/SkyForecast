22f1271161786b0123b3cd10f25a5170b96778f9
SkyWeb <skyweb331@gmail.com>
2017-02-09 13:07:19 -0500

    Unit Test Added

---
+++ test/server.js
@@ -0,0 +1,10 @@
+var chai = require('chai'),
+    chaiHttp = require('chai-http'),
+    server = require('../server/server.js'),
+    should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Node Test', function(){
+    it('should list all blobs on');
+});
