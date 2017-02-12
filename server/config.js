ee7f9db89b6a4f8defaf89135f5f0804970ee150
SkyWeb <skyweb331@gmail.com>
2017-02-12 16:24:18 -0500

    update gitignore

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {    
+  darkskyKey   : "27b3df883a81a491f54a09a44693bdaf",
+  googlePlacesKey   : "AIzaSyAT-KlUv1YZz6rJ7vpSGEc-ki7jAIUucqI",
+  localMongoURI: process.env.MONGDB_URI || 'mongodb://localhost:27017/skyforecast'
+ }
